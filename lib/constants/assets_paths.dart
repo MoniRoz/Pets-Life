886bcf8d1527822b179f11c82c6f267b95903bf8
mrozwadowska <Monika.Rozwadowska@grupawp.pl>
2021-01-08 14:56:53 +0100

    prepared  vets avatars

--- lib/constants/assets_paths.dart
+++ lib/constants/assets_paths.dart
@@ -1,2 +1,3 @@
 const String DEFAULT_PATH = "assets";
 const String IMAGES_PATH = "$DEFAULT_PATH/images";
+const String VECTORS_PATH = "$DEFAULT_PATH/vectors";
--- lib/screens/vets/vets.dart
+++ lib/screens/vets/vets.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'dart:math' as math;
+
+import 'package:pets_life/constants/assets_paths.dart';
 import 'package:pets_life/widgets/drawer/drawer.dart';
 
 class VetsScreen extends StatelessWidget {
@@ -11,8 +15,20 @@ class VetsScreen extends StatelessWidget {
         title: Text('List of vets'),
       ),
       drawer: AppDrawer(),
-      body: Center(
-        child: Text("Hello from vets list"),
+      body: GridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        children: List.generate(10, (index) {
+          final colorValue = (math.Random().nextDouble() * 0xFFFFFF).toInt();
+
+          return ClipOval(
+            child: Container(
+              color: Color(colorValue).withOpacity(1.0),
+              child: SvgPicture.asset('$VECTORS_PATH/avatar_${++index}.svg'),
+            ),
+          );
+        }),
       ),
     );
   }
